065237ac06e0c1ecab3177ecc847720b9d00fefb
Suzuki ryoto <s24g1079jw@s.chibakoudai.jp>
2024-11-18 19:51:20 +0900

    途中経過

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ Gitで管理する際，誰が変更したか明らかにするため，
 $ git config --global user.name "Suzuki Ryoto"
 $ git config --global user.email "suzuki@ryoto.co.jp"
 ```
-GitHubに修正，追加したファイルをアップロードしたい場合，次のコマンドをターミナルかそれに準じたソフトで実行する．
-なお，'コメント'の部分は変更理由や変更内容を書くためにある．
+GitHubに修正，追加したファイルをアップロードしたい場合，次のコマンドをターミナルで実行する．
+なお，'コメント'の部分は変更理由や変更内容を書くためにあるためそのままにしない．
 ```javascript
 git add .
 git commit -am 'コメント'
@@ -19,31 +19,36 @@ git push
 ```
 
 ## サーバーの起動について
+起動したいサーバーのプログラムが書かれているファイルがあるディレクトリで以下のように実行する．
+例として今回起動するファイルを「app.js」とする．
+```javascript
+node app.js
+```
 
-
-
-
-
-## このプログラムについて
-
-## ファイル一覧
+## じゃんけん
+### このプログラムについて
+このプログラムは，「グー，チョキ，パー」のいずれかを入力すると，コンピュータ側がランダムで出した手との勝敗を判定するプログラムである．勝敗の他に
+### ファイル一覧
 ファイル名 | 説明
 -|-
 app5.js | プログラム本体
 public/janken.html | じゃんけんの開始画面
 views/janken.ejs | 画面のテンプレート
 
-```javascript
-console.log( 'Hello' );
-```
-
-## 利用手順
-
-### じゃんけん
+### 利用手順
 1. ```app5.js```を起動する
 1. Webブラウザでlocalhost:8080/public/janken.htmlにアクセスする
 1. 自分の手を入力する
 
+## スロット
+### このプログラムについて
+### ファイル一覧
+### 利用手順
+
+## パズル
+### このプログラムについて
+### ファイル一覧
+### 利用手順
 
 
 
@@ -58,7 +63,6 @@ start["開始"];
 end1["終了"]
 if{"じゃんけんの勝敗"}
 win["試合数と勝数を1増やす"]
-loose["試合数を1増やす"]
 draw["試合数を1増やす"]
 
 
@@ -67,13 +71,56 @@ if -->|勝ち| win
 win --> end1
 if -->|引き分け| draw
 draw --> end1
-if --> |負け| loose
-loose --> end1
+if --> |負け| draw
 
 ```
 
+###　スロット
+
+```mermaid
+flowchart TD;
+
+start["開始"]
+end1["終了"]
+bet["賭け金の入力"]
+slot["3つの乱数を表示"]
+if{値が3つ揃っているか}
+result["現在の所持金表示"]
+win["賭け金増加"]
+lose["賭け金消失"]
 
 
+start --> bet
+bet --> slot
+slot --> if
+if --> |yes|win
+win --> result
+if --> |no|lose
+lose --> result
+result --> end1
+
+```
+
+###　パズル
+
+```mermaid
+flowchart TD;
+
+start["開始"]
+cube["パズルの生成"]
+button["ボタンを選択"]
+reverse["自身と両隣を反転"]
+if{パズルが揃っているか}
+clear["クリア表示"]
+end1["終了"]
 
 
-10/28
+start --> cube
+cube --> button
+button --> reverse
+reverse --> if
+if --> |yes|clear
+clear --> end1
+if --> |no|button
+
+```
--- app5.js
+++ app5.js
@@ -1,79 +1,282 @@
 const express = require("express");
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/hello1", (req, res) => {
   const message1 = "Hello world";
   const message2 = "Bon jour";
-  res.render('show', { greet1:message1, greet2:message2});
+  res.render("show", { greet1: message1, greet2: message2 });
 });
 
 app.get("/hello2", (req, res) => {
-  res.render('show', { greet1:"Hello world", greet2:"Bon jour"});
+  res.render("show", { greet1: "Hello world", greet2: "Bon jour" });
 });
 
 app.get("/icon", (req, res) => {
-  res.render('icon', { filename:"./public/Apple_logo_black.svg", alt:"Apple Logo"});
+  res.render("icon", {
+    filename: "./public/Apple_logo_black.svg",
+    alt: "Apple Logo",
+  });
 });
 
 app.get("/luck", (req, res) => {
-  const num = Math.floor( Math.random() * 6 + 1 );
-  let luck = '';
-  if( num==1 ) luck = '大吉';
-  else if( num==2 ) luck = '中吉';
-  console.log( 'あなたの運勢は' + luck + 'です' );
-  res.render( 'luck', {number:num, luck:luck} );
+  const num = Math.floor(Math.random() * 6 + 1);
+  let luck = "";
+  if (num == 1) luck = "大吉";
+  else if (num == 2) luck = "中吉";
+  console.log("あなたの運勢は" + luck + "です");
+  res.render("luck", { number: num, luck: luck });
 });
 
 app.get("/janken", (req, res) => {
   let hand = req.query.hand;
-  let win = Number( req.query.win );
-  let total = Number( req.query.total );
-  console.log( {hand, win, total});
-  const num = Math.floor( Math.random() * 3 + 1 );
-  let cpu = '';
-  let judgement = '';
-  if( num==1 ) cpu = 'グー';
-  else if( num==2 ) cpu = 'チョキ';
-  else cpu = 'パー';
+  let win = Number(req.query.win);
+  let total = Number(req.query.total);
+  console.log({ hand, win, total });
+  const num = Math.floor(Math.random() * 3 + 1);
+  let cpu = "";
+  let judgement = "";
+  if (num == 1) cpu = "グー";
+  else if (num == 2) cpu = "チョキ";
+  else cpu = "パー";
   total += 1;
-  if(isNaN(total)){
+  if (isNaN(total)) {
     total = 0;
   }
-  if(isNaN(win)){
+  if (isNaN(win)) {
     win = 0;
   }
 
-  if(hand=='グー'){
-    if(cpu == 'グー') judgement = '引き分け';
-    else if(cpu == 'チョキ'){
-      judgement = '勝ち' 
-      win += 1;}
-    else if(cpu == 'パー')judgement = '負け';
-  }else if(hand=='チョキ'){
-    if(cpu == 'グー') judgement = '負け';
-    else if(cpu == 'チョキ')judgement = '引き分け';
-    else if(cpu == 'パー'){
-      judgement = '勝ち'
-      win += 1;}
-  }else if(hand=='パー'){
-    if(cpu == 'グー'){
-      judgement = '勝ち'
-      win += 1}
-    else if(cpu == 'チョキ')judgement = '負け';
-    else if(cpu == 'パー')judgement = '引き分け';
+  if (hand == "グー") {
+    if (cpu == "グー") judgement = "引き分け";
+    else if (cpu == "チョキ") {
+      judgement = "勝ち";
+      win += 1;
+    } else if (cpu == "パー") judgement = "負け";
+  } else if (hand == "チョキ") {
+    if (cpu == "グー") judgement = "負け";
+    else if (cpu == "チョキ") judgement = "引き分け";
+    else if (cpu == "パー") {
+      judgement = "勝ち";
+      win += 1;
+    }
+  } else if (hand == "パー") {
+    if (cpu == "グー") {
+      judgement = "勝ち";
+      win += 1;
+    } else if (cpu == "チョキ") judgement = "負け";
+    else if (cpu == "パー") judgement = "引き分け";
   }
-  
+
+  if(hand == "リセット"){
+    total =0;
+    win =0;
+    hand ="";
+  }
+
   const display = {
     your: hand,
     cpu: cpu,
     judgement: judgement,
     win: win,
-    total: total
+    total: total,
+  };
+  res.render("janken", display);
+});
+
+app.get("/slot", (req, res) => {
+  let bet = Number(req.query.bet_f);
+  let money = Number(req.query.money_f);
+  let game = req.query.game_f;
+  let reset = Number(req.query.reset);
+  let luck = "";
+  let num = 0;
+  let mum = 0;
+  let lum = 0;
+  console.log({ bet, money, game });
+  if (isNaN(bet)||reset == 1) {
+    bet = 0;
   }
-  res.render( 'janken', display );
+  if (isNaN(money)||reset == 1) {
+    money = 1000;
+  }
+  reset = 0;
+  console.log({ bet, money });
+
+  if (bet > 0) {
+    if (money - bet >= 0) {
+      num = Math.floor(Math.random() * 5 + 1);
+      mum = Math.floor(Math.random() * 5 + 1);
+      lum = Math.floor(Math.random() * 5 + 1);
+
+      if (num == mum) {
+        if (mum == lum) {
+          if (lum == num) luck = "あたり";
+          else luck = "はずれ";
+        } else luck = "はずれ";
+      } else luck = "はずれ";
+
+      money = money - bet;
+      if (luck == "あたり") {
+        bet = bet * 10;
+      } else {
+        bet = bet * 0;
+      }
+      if (money == 0) {
+        game = "Game over";
+      } else game = "　";
+      money = money + bet;
+    } else {
+      game = "所持コインよりも少ない値を入力してください";
+      luck = "Error";
+    }
+  } else {
+    game = "1以上の整数を入力してください";
+    luck = "Error";
+  }
+  res.render("slot", {
+    number1: num,
+    number2: mum,
+    number3: lum,
+    luck: luck,
+    bet_f: bet,
+    money_f: money,
+    game_f: game,
+  });
+});
+
+app.get("/puzzle", (req, res) => {
+  let count = Number(req.query.count);
+  let reset = Number(req.query.reset);
+  let judge = Number(req.query.button);
+  let game_f = Number(req.query.game_f);
+  let p1 = Number(req.query.p1);
+  let p2 = Number(req.query.p2);
+  let p3 = Number(req.query.p3);
+  let p4 = Number(req.query.p4);
+  let p5 = Number(req.query.p5);
+  let ps1 = 0;
+  let ps2 = 0;
+  let ps3 = 0;
+  let ps4 = 0;
+  let ps5 = 0;
+  console.log({ judge });
+  console.log({ p1, p2, p3, p4, p5, reset });
+  if (isNaN(count) || reset == 1) {
+    count = 0;
+    reset = 0;
+  }
+
+  if (count == 0) {
+    p1 = 0;
+    p2 = 0;
+    p3 = 0;
+    p4 = 0;
+    p5 = 0;
+    while (p1 == p2 && p2 == p3 && p3 == p4 && p4 == p5 && p5 == p1) {
+      p1 = Math.floor(Math.random() * 2 + 1);
+      p2 = Math.floor(Math.random() * 2 + 1);
+      p3 = Math.floor(Math.random() * 2 + 1);
+      p4 = Math.floor(Math.random() * 2 + 1);
+      p5 = Math.floor(Math.random() * 2 + 1);
+    }
+  }
+
+  if (judge == 1) {
+    if (p5 == 1) {
+      p5 = 2;
+    } else p5 = 1;
+    if (p1 == 1) {
+      p1 = 2;
+    } else p1 = 1;
+    if (p2 == 1) {
+      p2 = 2;
+    } else p2 = 1;
+  }
+  if (judge == 2) {
+    if (p1 == 1) {
+      p1 = 2;
+    } else p1 = 1;
+    if (p2 == 1) {
+      p2 = 2;
+    } else p2 = 1;
+    if (p3 == 1) {
+      p3 = 2;
+    } else p3 = 1;
+  }
+  if (judge == 3) {
+    if (p2 == 1) {
+      p2 = 2;
+    } else p2 = 1;
+    if (p3 == 1) {
+      p3 = 2;
+    } else p3 = 1;
+    if (p4 == 1) {
+      p4 = 2;
+    } else p4 = 1;
+  }
+  if (judge == 4) {
+    if (p3 == 1) {
+      p3 = 2;
+    } else p3 = 1;
+    if (p4 == 1) {
+      p4 = 2;
+    } else p4 = 1;
+    if (p5 == 1) {
+      p5 = 2;
+    } else p5 = 1;
+  }
+  if (judge == 5) {
+    if (p4 == 1) {
+      p4 = 2;
+    } else p4 = 1;
+    if (p5 == 1) {
+      p5 = 2;
+    } else p5 = 1;
+    if (p1 == 1) {
+      p1 = 2;
+    } else p1 = 1;
+  }
+
+  if (p1 == 1) {
+    ps1 = "◯";
+  } else ps1 = "☓";
+  if (p2 == 1) {
+    ps2 = "◯";
+  } else ps2 = "☓";
+  if (p3 == 1) {
+    ps3 = "◯";
+  } else ps3 = "☓";
+  if (p4 == 1) {
+    ps4 = "◯";
+  } else ps4 = "☓";
+  if (p5 == 1) {
+    ps5 = "◯";
+  } else ps5 = "☓";
+
+  if (p1 == p2 && p2 == p3 && p3 == p4 && p4 == p5 && p5 == p1) {
+    game_f = "Game clear";
+  } else game_f = "Play game";
+
+  count += 1;
+
+  res.render("puzzle", {
+    game_f: game_f,
+    p1: p1,
+    p2: p2,
+    p3: p3,
+    p4: p4,
+    p5: p5,
+    ps1: ps1,
+    ps2: ps2,
+    ps3: ps3,
+    ps4: ps4,
+    ps5: ps5,
+    count: count,
+    reset: reset,
+  });
 });
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
